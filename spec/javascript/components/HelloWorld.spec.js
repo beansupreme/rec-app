017201395c6c2313631dc9827675fb1ac44fdbf5
Andy Whillans <andy.whillans@gmail.com>
2018-06-13 19:42:36 -0400

    Get Jest tests passing for sample components, install Enzyme to help with testing

---
+++ spec/javascript/components/HelloWorld.spec.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { shallow } from 'enzyme';
+import HelloWorld from 'components/HelloWorld';
+
+
+describe('HelloWorld component', () => {
+  describe('when the greeting and number are passed', () => {
+    it('renders the greeting and number multiplied by 3', () => {
+      const wrapper = shallow(<HelloWorld greeting="Bonjour!" favoriteNumber={3}/>);
+      expect(wrapper.text()).toBe('Greeting: Bonjour! Favorite Number: 9');
+    })
+  })
+})
---
+++ spec/javascript/packs/hello_react.spec.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { shallow, mount, render } from 'enzyme';
+import Hello from 'packs/hello_react';
+
+describe('HelloReact component', () => {
+  describe('when a name is given as a prop', () => {
+    it('renders the given name!', () => {
+      const wrapper = shallow(<Hello name="Caique"/>);
+      expect(wrapper.text()).toBe('Hello Caique!');
+    })
+  })
+
+  describe('when no name is given', () => {
+    it('renders Hello David!', () => {
+      const wrapper = shallow(<Hello />);
+      expect(wrapper.text()).toBe('Hello David!');
+    })
+  })
+})
