f6706f643be5ffa2d1de7c4b25f7d944b60f5fa5
Andy Whillans <andy.whillans@gmail.com>
2018-06-14 13:48:12 -0400

    Create first snapshot test for Contact form

--- spec/javascript/components/ContactForm.spec.js
+++ spec/javascript/components/ContactForm.spec.js
@@ -1,10 +1,23 @@
-import React from 'react'
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
 import { shallow, render } from 'enzyme';
 import ContactForm from 'components/ContactForm';
+
+import renderer from 'react-test-renderer';
 
 describe('<ContactForm/>', () => {
   it('shows a greeting message', () => {
     const wrapper = render(<ContactForm />);
     expect(wrapper.text()).toContain('Add your info for class updates!');
+  });
+
+  it('renders as expected', () => {
+    const tree = renderer.create(<ContactForm />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<ContactForm />, div);
   });
 });
