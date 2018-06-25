d147841e49572fa7335be738fe7c7ba76fbc61f2
Andy Whillans <andy.whillans@gmail.com>
2018-06-25 15:00:26 -0400

    Remove old server side rendered guest form

--- spec/features/guests_spec.rb
+++ spec/features/guests_spec.rb
@@ -4,24 +4,7 @@ describe 'visiting the home page' do
   it 'allows the user to add themselves as a contact', js: true do
     visit '/'
 
-    expect(page).to have_content('Want to sign up for class updates? Add your info here!')
-
-    within('#contact-form-card') do
-      click_on 'Add me to the contact list!'
-
-      expect(page).to have_content('Name can\'t be blank')
-
-      fill_in 'Name', with: 'Mister Rogers'
-      fill_in 'Email', with: 'wontyoubemyfriend@gmail.com'
-      fill_in 'Telephone', with: '123-456-8912'
-      fill_in 'Mailing address', with: 'Mister Rogers\' Neighborhood'
-
-      click_on 'Add me to the contact list!'
-      expect(page).to have_content('Thanks for submitting your info!')
-
-      mr_rogers = Contact.find_by_name('Mister Rogers')
-      expect(mr_rogers).to be_present
-    end
+    expect(page).to have_content('Add your info for class updates!')
 
     within('#add-contact-form') do
       click_on 'Add Contact info'
